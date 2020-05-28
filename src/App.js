6376c157447229f04a4134459667d03f6e10a0e9
Mert Demirok <mertdemirok80@gmail.com>
2020-05-28 08:08:51 +0200

    feat:fetch all books from API

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import WantToRead from "./components/WantToRead";
 import Read from "./components/Read";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import {getAll} from "./BooksAPI"
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books:[]
   };
+
+  componentDidMount (){
+    getAll().then(books => 
+      {
+      this.setState({books})
+      console.log(books)
+      })
+    console.log(this.state.books)
+  }
 
   render() {
     const App = () => {
