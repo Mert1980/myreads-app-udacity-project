7740cf262274bf0671e0706f9030341f424356ef
Mert Demirok <mertdemirok80@gmail.com>
2020-06-03 23:10:23 +0200

    Updates

---
+++ precache-manifest.2d5bf09a1bd3ee0abc34d9b072502906.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d687810e950c296e1de",
+    "url": "/myreads-app-udacity-project/static/css/main.3ea568e1.chunk.css"
+  },
+  {
+    "revision": "6d687810e950c296e1de",
+    "url": "/myreads-app-udacity-project/static/js/main.6d687810.chunk.js"
+  },
+  {
+    "revision": "8f6e13c0d5cbc95fba8b",
+    "url": "/myreads-app-udacity-project/static/js/1.8f6e13c0.chunk.js"
+  },
+  {
+    "revision": "5f295955400ab98f820b",
+    "url": "/myreads-app-udacity-project/static/js/runtime~main.5f295955.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/myreads-app-udacity-project/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/myreads-app-udacity-project/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/myreads-app-udacity-project/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "cacf40a4d226091be7bce1ea5e1a0f28",
+    "url": "/myreads-app-udacity-project/index.html"
+  }
+];
